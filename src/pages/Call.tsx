2208f25e9753d12d98b030661e7d0b71f2da472d
blueocean <dev.freshworks@gmail.com>
2024-12-15 02:31:26 -0500

    [update] call page

--- src/pages/Call.tsx
+++ src/pages/Call.tsx
@@ -63,6 +63,7 @@ const Call = () => {
       );
       const data: TokenResponse = response.data;
       const newDevice = new Device(data.token, {
+        identity: aivioPhoneNumber,
         codecPreferences: [Connection.Codec.PCMU, Connection.Codec.Opus],
         fakeLocalDTMF: true,
         enableRingingState: true,
